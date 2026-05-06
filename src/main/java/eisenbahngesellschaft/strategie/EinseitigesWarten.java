54f2d79b01c1c0211a39ad92803493f34be693e4
Mohammed Alassane <mohammed.alassane@gothaer.de>
2026-05-06 16:00:36 +0200

    UML

--- src/main/java/eisenbahngesellschaft/strategie/EinseitigesWarten.java
+++ src/main/java/eisenbahngesellschaft/strategie/EinseitigesWarten.java
@@ -113,7 +113,7 @@ public class EinseitigesWarten implements FahrplanStrategie {
                 aktuelleStrecke.setKollision(false);
             }
 
-            //TODO: Änderung vs Konzept
+            //TODO: Änderung vs Konzept markieren
 
             // 3. Prüfung auf Einhaltung der Sicherheitswartezeit
             // Auch ohne Streckenkollision muss die Sicherheitswartezeit im Bahnhof gelten
